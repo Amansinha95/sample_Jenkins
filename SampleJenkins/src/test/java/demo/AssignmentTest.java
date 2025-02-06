4dfbc5673a8b72c657de3a1a910b0e928ad9a9ff
dell <dell@DESKTOP-M1U6SU2>
2025-02-06 18:39:31 +0530

    sample jenkins

---
+++ SampleJenkins/src/test/java/demo/AssignmentTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AssignmentTest {
+@Test
+public void jenkins() {
+	Reporter.log("Assignment class loaded",true);
+}
+}
